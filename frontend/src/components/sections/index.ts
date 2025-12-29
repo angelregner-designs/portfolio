49f788be6ff8cfb667727fe4e65f60c58b460cf9
Christopher Regner <chrisregner.com@gmail.com>
2025-12-29 12:24:12 +0800

    Remove temp claude code

--- frontend/src/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as About } from './About'
-export { default as Contact } from './Contact'
-export { default as Footer } from './Footer'
-export { default as Hero } from './Hero'
-export { default as Portfolio } from './Portfolio'
-export { default as Testimonials } from './Testimonials'
-export { default as WhyIDesign } from './WhyIDesign'
